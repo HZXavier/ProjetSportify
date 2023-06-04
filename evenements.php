7cd09a36fe128fbe326782cfae6a94ccfe931296
Léo <103055096+xX-Alucard-Xx@users.noreply.github.com>
2023-06-04 22:44:52 +0200

    Update evenements.php

--- evenements.php
+++ evenements.php
@@ -5,6 +5,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>EVENEMENTS</title>
 	<link rel="stylesheet" href="style.css">
+	<?php
+session_start(); ?>
 </head>
 <body>
 	<div class="wrapper">
